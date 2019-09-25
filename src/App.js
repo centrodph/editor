bec829ec8389826d87d9e5d49b3bd426924bc734
Gerardo Perrucci <centrodph@gmail.com>
2019-09-25 08:34:05 -0300

    basic context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./assets/App.css";
 import { Toolbar, Header, Footer, Content } from "./components";
+import { EditorContext, initialState, reducer } from "./utils";
 
 function App() {
+  const [editor, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <Toolbar />
-        <Content />
-        <Footer />
-      </header>
-    </div>
+    <EditorContext.Provider value={{ editor, dispatch }}>
+      <div className="App">
+        <header className="App-header">
+          <Header />
+          <Toolbar />
+          <Content />
+          <Footer />
+        </header>
+      </div>
+    </EditorContext.Provider>
   );
 }
 
---
+++ src/utils/state.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const EditorContext = React.createContext(null);
+
+export const ACTIONS = {  
+  SELECT_WORD: "SELECT_WORD",
+  TOGGLE_STYLE: "TOGGLE_STYLE"
+};
+
+export const initialState = { words: [], styles: {}, selected: null };
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.SELECT_WORD:
+      return { ...state, selected: action.payload };
+    case ACTIONS.TOGGLE_STYLE:
+      const { payload } = action;
+      const { styles } = { ...state };
+      if (!styles[payload.type]) styles[payload.type] = {};
+      if (styles[payload.type] && styles[payload.type][payload.index])
+        delete styles[payload.type][payload.index];
+      else styles[payload.type][payload.index] = true;
+      return { ...state, styles: { ...state.styles } };
+    default:
+      throw new Error();
+  }
+}
